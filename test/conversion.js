5c49ba92d1c8bdeade6cd0ca9789b4720fcc3089
kchapelier <kevin.chapelier@gmail.com>
2014-07-09 13:03:42 +0200

    fix issue last commit

--- test/conversion.js
+++ test/conversion.js
@@ -6,55 +6,20 @@ var Mathp = require('../index.js');
 
 //TODO more loose check on the float values as their precision is dependant of external factors
 
-describe('Distance', function(){
-	describe('#euclideanDistance()', function() {
-		it('', function() {
-			Mathp.euclideanDistance(0, 0, 0, 0).should.equal(0);
-			Mathp.euclideanDistance(0, 0, 0, 1).should.equal(1);
-			Mathp.euclideanDistance(0, 0, 1, 0).should.equal(1);
-			Mathp.euclideanDistance(0, 0, 1, 1).should.equal(1.4142135623730951);
-			Mathp.euclideanDistance(0, 0, 0, -1).should.equal(1);
-			Mathp.euclideanDistance(0, 0, -1, 0).should.equal(1);
-			Mathp.euclideanDistance(0, 0, -1, -1).should.equal(1.4142135623730951);
-		});
-
-		it('', function() {
-			Mathp.euclideanDistance(2, 10.5, 2, 5).should.equal(5.5);
-			Mathp.euclideanDistance(-10, -10, 10, 10).should.equal(28.284271247461902);
+describe('Conversion', function(){
+	describe('#toRadians()', function() {
+		it('should make the correct conversion', function() {
+			Mathp.toRadians(0).should.equal(0);
+			Mathp.toRadians(10).should.equal(0.17453292519943295);
+			Mathp.toRadians(-10).should.equal(-0.17453292519943295);
 		});
 	});
 
-	describe('#manhattanDistance()', function() {
-		it('', function() {
-			Mathp.manhattanDistance(0, 0, 0, 0).should.equal(0);
-			Mathp.manhattanDistance(0, 0, 0, 1).should.equal(1);
-			Mathp.manhattanDistance(0, 0, 1, 0).should.equal(1);
-			Mathp.manhattanDistance(0, 0, 1, 1).should.equal(2);
-			Mathp.manhattanDistance(0, 0, 0, -1).should.equal(1);
-			Mathp.manhattanDistance(0, 0, -1, 0).should.equal(1);
-			Mathp.manhattanDistance(0, 0, -1, -1).should.equal(2);
-		});
-
-		it('', function() {
-			Mathp.manhattanDistance(2, 10.5, 2, 5).should.equal(5.5);
-			Mathp.manhattanDistance(-10, -10, 10, 10).should.equal(40);
-		});
-	});
-
-	describe('#chebyshevDistance()', function() {
-		it('', function() {
-			Mathp.chebyshevDistance(0, 0, 0, 0).should.equal(0);
-			Mathp.chebyshevDistance(0, 0, 0, 1).should.equal(1);
-			Mathp.chebyshevDistance(0, 0, 1, 0).should.equal(1);
-			Mathp.chebyshevDistance(0, 0, 1, 1).should.equal(1);
-			Mathp.chebyshevDistance(0, 0, 0, -1).should.equal(1);
-			Mathp.chebyshevDistance(0, 0, -1, 0).should.equal(1);
-			Mathp.chebyshevDistance(0, 0, -1, -1).should.equal(1);
-		});
-
-		it('', function() {
-			Mathp.chebyshevDistance(2, 10.5, 2, 5).should.equal(5.5);
-			Mathp.chebyshevDistance(-10, -10, 10, 10).should.equal(20);
+	describe('#toDegrees()', function() {
+		it('should make the correct conversion', function() {
+			Mathp.toDegrees(0).should.equal(0);
+			Mathp.toDegrees(10).should.equal(572.9577951308232);
+			Mathp.toDegrees(-10).should.equal(-572.9577951308232);
 		});
 	});
 });
--- test/distance.js
+++ test/distance.js
@@ -4,54 +4,57 @@ var should = require('chai').should();
 
 var Mathp = require('../index.js');
 
-describe('General', function(){
-	describe('#lerp()', function() {
-		it('should make a proper linear interpolation', function() {
-			Mathp.lerp(0, 10, 0.2).should.equal(2);
-			Mathp.lerp(0, 10, 0).should.equal(0);
-			Mathp.lerp(0, 10, 1).should.equal(10);
-			Mathp.lerp(5, -5, 0.2).should.equal(3);
+//TODO more loose check on the float values as their precision is dependant of external factors
+
+describe('Distance', function(){
+	describe('#euclideanDistance()', function() {
+		it('should work correctly on all axis', function() {
+			Mathp.euclideanDistance(0, 0, 0, 0).should.equal(0);
+			Mathp.euclideanDistance(0, 0, 0, 1).should.equal(1);
+			Mathp.euclideanDistance(0, 0, 1, 0).should.equal(1);
+			Mathp.euclideanDistance(0, 0, 1, 1).should.equal(1.4142135623730951);
+			Mathp.euclideanDistance(0, 0, 0, -1).should.equal(1);
+			Mathp.euclideanDistance(0, 0, -1, 0).should.equal(1);
+			Mathp.euclideanDistance(0, 0, -1, -1).should.equal(1.4142135623730951);
 		});
 
-		it('should never return a value outside of the bounds', function() {
-			Mathp.lerp(0, 10, 100).should.equal(10);
-			Mathp.lerp(0, 10, -100).should.equal(0);
-		});
-	});
-
-	describe('#limit()', function() {
-		it('should limit the given value to the bounds', function() {
-			Mathp.limit(12, 10, 20).should.equal(12);
-			Mathp.limit(1, 10, 20).should.equal(10);
-			Mathp.limit(20.1, 10, 20).should.equal(20);
-		});
-
-		it('should accept any order for the bounds', function() {
-			Mathp.limit(1, 10, 20).should.equal(10);
-			Mathp.limit(1, 20, 10).should.equal(10);
+		it('should works correctly with float values', function() {
+			Mathp.euclideanDistance(2, 10.5, 2, 5).should.equal(5.5);
+			Mathp.euclideanDistance(-10.2, -10, 10, 10).should.equal(28.426044395940846);
 		});
 	});
 
-	describe('#scale()', function() {
-		it('should scale accordingly to the bounds', function() {
-			Mathp.scale(0, 0, 1, -1, 1).should.equal(-1);
-			Mathp.scale(0.5, 0, 1, -1, 1).should.equal(0);
-			Mathp.scale(1, 0, 1, -1, 1).should.equal(1);
+	describe('#manhattanDistance()', function() {
+		it('should work correctly on all axis', function() {
+			Mathp.manhattanDistance(0, 0, 0, 0).should.equal(0);
+			Mathp.manhattanDistance(0, 0, 0, 1).should.equal(1);
+			Mathp.manhattanDistance(0, 0, 1, 0).should.equal(1);
+			Mathp.manhattanDistance(0, 0, 1, 1).should.equal(2);
+			Mathp.manhattanDistance(0, 0, 0, -1).should.equal(1);
+			Mathp.manhattanDistance(0, 0, -1, 0).should.equal(1);
+			Mathp.manhattanDistance(0, 0, -1, -1).should.equal(2);
 		});
 
-		it('should never return a value outside of the destination bounds', function() {
-			Mathp.scale(100, 0, 1, -1, 1).should.equal(1);
-			Mathp.scale(-1, 0, 1, -1, 1).should.equal(-1);
+		it('should works correctly with float values', function() {
+			Mathp.manhattanDistance(2, 10.5, 2, 5).should.equal(5.5);
+			Mathp.manhattanDistance(-10.2, -10, 10, 10).should.equal(40.2);
+		});
+	});
+
+	describe('#chebyshevDistance()', function() {
+		it('should work correctly on all axis', function() {
+			Mathp.chebyshevDistance(0, 0, 0, 0).should.equal(0);
+			Mathp.chebyshevDistance(0, 0, 0, 1).should.equal(1);
+			Mathp.chebyshevDistance(0, 0, 1, 0).should.equal(1);
+			Mathp.chebyshevDistance(0, 0, 1, 1).should.equal(1);
+			Mathp.chebyshevDistance(0, 0, 0, -1).should.equal(1);
+			Mathp.chebyshevDistance(0, 0, -1, 0).should.equal(1);
+			Mathp.chebyshevDistance(0, 0, -1, -1).should.equal(1);
 		});
 
-		it('should work with inverted min and max destination bounds', function() {
-			Mathp.scale(1, 0, 1, 1, -1).should.equal(-1);
-			Mathp.scale(0, 0, 1, 1, -1).should.equal(1);
-		});
-
-		it('should work with inverted min and max original bounds', function() {
-			Mathp.scale(1, 1, 0, -1, 1).should.equal(-1);
-			Mathp.scale(0, 1, 0, -1, 1).should.equal(1);
+		it('should works correctly with float values', function() {
+			Mathp.chebyshevDistance(2, 10.5, 2, 5).should.equal(5.5);
+			Mathp.chebyshevDistance(-10.2, -10, 10, 10).should.equal(20.2);
 		});
 	});
 });
