bb65615f1df48b200864ca87c8681328484cda6d
kchapelier <kevin.chapelier@gmail.com>
2014-07-10 12:19:27 +0200

    tests on 'means'

---
+++ test/mean.js
@@ -0,0 +1,81 @@
+"use strict";
+
+var should = require('chai').should();
+
+var Mathp = require('../index.js');
+
+//TODO more loose check on the float values as their precision is dependant of external factors
+
+describe('Mean', function(){
+	describe('#amean()', function() {
+		it('should return 0 on an empty array and with zero arg', function() {
+			Mathp.amean().should.equal(0);
+			Mathp.amean([]).should.equal(0);
+		});
+
+		it('should return NaN when any of the values is NaN', function() {
+			Mathp.amean([0, 1, NaN]).should.eql(NaN);
+			Mathp.amean([0, 1, "Toto"]).should.eql(NaN);
+		});
+
+		it('', function() {
+			Mathp.amean([0, 1, 2]).should.equal(1);
+			Mathp.amean([1, 2, 3]).should.equal(2);
+			Mathp.amean([-20, -5, 10]).should.equal(-5);
+		});
+	});
+
+	describe('#gmean()', function() {
+		it('should return 0 on an empty array and with zero arg', function() {
+			Mathp.gmean().should.equal(0);
+			Mathp.gmean([]).should.equal(0);
+		});
+
+		it('should return NaN when any of the values is NaN', function() {
+			Mathp.gmean([0, 1, NaN]).should.eql(NaN);
+			Mathp.gmean([0, 1, "Toto"]).should.eql(NaN);
+		});
+
+		it('', function() {
+			Mathp.gmean([0, 1, 2]).should.equal(0);
+			Mathp.gmean([1, 2, 3]).should.equal(1.8171205928321397);
+			Mathp.gmean([-20, -5, 10]).should.equal(9.999999999999998);
+		});
+	});
+
+	describe('#hmean()', function() {
+		it('should return 0 on an empty array and with zero arg', function() {
+			Mathp.hmean().should.equal(0);
+			Mathp.hmean([]).should.equal(0);
+		});
+
+		it('should return NaN when any of the values is NaN', function() {
+			Mathp.hmean([0, 1, NaN]).should.eql(NaN);
+			Mathp.hmean([0, 1, "Toto"]).should.eql(NaN);
+		});
+
+		it('', function() {
+			Mathp.hmean([0, 1, 2]).should.equal(0);
+			Mathp.hmean([1, 2, 3]).should.equal(1.6363636363636365);
+			Mathp.hmean([-20, -5, 10]).should.equal(-20);
+		});
+	});
+
+	describe('#rms()', function() {
+		it('should return 0 on an empty array and with zero arg', function() {
+			Mathp.rms().should.equal(0);
+			Mathp.rms([]).should.equal(0);
+		});
+
+		it('should return NaN when any of the values is NaN', function() {
+			Mathp.rms([0, 1, NaN]).should.eql(NaN);
+			Mathp.rms([0, 1, "Toto"]).should.eql(NaN);
+		});
+
+		it('', function() {
+			Mathp.rms([0, 1, 2]).should.equal(1.2909944487358056);
+			Mathp.rms([1, 2, 3]).should.equal(2.160246899469287);
+			Mathp.rms([-20, -5, 10]).should.equal(13.228756555322953);
+		});
+	});
+});
