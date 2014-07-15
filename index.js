f13f7420b18a83754dbc398342c22bdacdad0d5d
kchapelier <kevin.chapelier@gmail.com>
2014-07-15 13:43:34 +0200

    cleaner object composing

--- index.js
+++ index.js
@@ -1,38 +1,33 @@
 var Mathp = {};
 
-//TODO this breaks most IDE features and should be replaced at a later time
-var importFunction = function(module) {
-	for(var key in module) {
-		if(module.hasOwnProperty(key) && !Mathp.hasOwnProperty(key)) {
-			Mathp[key] = module[key];
+var importFunctions = function(obj, functions) {
+	functions = functions || Object.keys(obj);
+
+	for(var i = 0; i < functions.length; i++) {
+		var key = functions[i];
+		if(obj.hasOwnProperty(key) && !Mathp.hasOwnProperty(key)) {
+			Mathp[key] = obj[key];
 		}
 	}
 };
 
-//proxy all the methods and properties of the Math object
-
-[
+var mathProperties = [
 	'E', 'PI', 'LN2', 'LN10', 'LOG2E', 'LOG10E', 'SQRT1_2', 'SQRT2',
 	'abs', 'acos', 'acosh', 'asin', 'asinh', 'atan', 'atanh', 'atan2',
 	'cbrt', 'ceil', 'clz32', 'cos', 'cosh', 'exp', 'floor', 'fround',
 	'hypot', 'imul', 'log', 'log1p', 'log10', 'log2', 'max', 'min', 'pow',
 	'random', 'round', 'sign', 'sin', 'sinh', 'sqrt', 'tan', 'tanh', 'trunc'
-].forEach(function(key) {
-	if(Math.hasOwnProperty(key) && !Mathp.hasOwnProperty(key)) {
-		Mathp[key] = Math[key];
-	}
-});
+];
 
-//import all the modules
-
-importFunction(require('./lib/polyfill'));
-importFunction(require('./lib/check'));
-importFunction(require('./lib/properties'));
-importFunction(require('./lib/general'));
-importFunction(require('./lib/trigonometry'));
-importFunction(require('./lib/mean'));
-importFunction(require('./lib/distance'));
-importFunction(require('./lib/conversion'));
-importFunction(require('./lib/shaper'));
+importFunctions(Math, mathProperties);
+importFunctions(require('./lib/polyfill'));
+importFunctions(require('./lib/check'));
+importFunctions(require('./lib/properties'));
+importFunctions(require('./lib/general'));
+importFunctions(require('./lib/trigonometry'));
+importFunctions(require('./lib/mean'));
+importFunctions(require('./lib/distance'));
+importFunctions(require('./lib/conversion'));
+importFunctions(require('./lib/shaper'));
 
 module.exports = Mathp;
