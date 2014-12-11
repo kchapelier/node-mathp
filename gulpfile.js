2c763e99a1f92bb00b66c71f19c891caf6004f3d
kchapelier <kevin.chapelier@gmail.com>
2014-12-11 10:14:03 +0100

    mocha, jscs and jshint as gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,31 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat-util');
-var fs = require('fs');
+var gulp = require('gulp'),
+	concat = require('gulp-concat-util'),
+	fs = require('fs'),
+	jscs = require('gulp-jscs'),
+	jshint = require('gulp-jshint'),
+	stylish = require('jshint-stylish'),
+	mocha = require('gulp-mocha');
+
+var files = ['./lib/*.js'];
+
+gulp.task('lint', function() {
+	return gulp
+		.src(files)
+		.pipe(jshint('.jshintrc'))
+		.pipe(jshint.reporter(stylish));
+});
+
+gulp.task('codestyle', function() {
+	return gulp
+		.src(files)
+		.pipe(jscs());
+});
+
+gulp.task('test', function() {
+	return gulp
+		.src([ './test/*.js' ])
+		.pipe(mocha({ reporter: 'spec' }));
+});
 
 gulp.task('build', function() {
 	gulp.src(['./lib/*.js', './index.js'])
@@ -11,7 +36,7 @@ gulp.task('build', function() {
 					.replace(/\/[\r\n]*\* Object elements retrieval \*\/[^]+\/\* Object elements retrieval end \*\/[\r\n]*/g, '')
 					// Remove all "use strict";
 					.replace(/[\r\n]*.*"use strict";?.*[\r\n]*/g, '\n')
-					// Remoe all module.exports
+					// Remove all module.exports
 					.replace(/[\r\n]*.*module.exports.*[\r\n]*/g, '\n')
 					// Add a tab at the beginning of every lines
 					.replace(/(^|\r*\n)/g, '\n\t')
