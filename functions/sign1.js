cc72ee910e2c691407fba20ebd46e9a568fd8677
kchapelier <kevin.chapelier@gmail.com>
2015-01-04 12:25:54 +0100

    remove unnecessary comparaison with Infinity

--- functions/sign1.js
+++ functions/sign1.js
@@ -5,7 +5,7 @@ module.exports = function sign1 (value) {
 
     if (isNaN(value)) {
         return NaN;
-    } else if (value > 0 || isPositiveZero(value) || value === Infinity) {
+    } else if (value > 0 || isPositiveZero(value)) {
         return 1;
     } else {
         return -1;
